819843a1d78e266c4f3cc0ef68a83c028811b2c2
Shubham Tadas <shubhamtadas828@gmail.com>
2022-03-24 16:23:39 +0530

    first commit

---
+++ BeanAutowiring ByConstructor/src/org/com/Capital.java
@@ -0,0 +1,10 @@
+package org.com;
+ public class Capital {
+        String capitalName;
+        public String getCapitalName() {
+              return capitalName;
+       }
+        public void setCapitalName(String capitalName) {
+              this.capitalName = capitalName;
+       }
+}
