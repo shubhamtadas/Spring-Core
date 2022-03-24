b30e066ad6b2b9e34a2887bca2c905051e43584d
Shubham Tadas <shubhamtadas828@gmail.com>
2022-03-24 16:18:31 +0530

    first commit

---
+++ springIOC/src/org/com/Country.java
@@ -0,0 +1,17 @@
+package org.com;
+public class Country {
+    String countryName;
+    Capital capital;
+    public String getCountryName() {
+        return countryName;
+    }
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+    public Capital getCapital() {
+        return capital;
+    }
+    public void setCapital(Capital capital) {
+        this.capital = capital;
+    }
+}
---
+++ springIOC/src/org/com/SetterMethodMain.java
@@ -0,0 +1,16 @@
+package org.com;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+public class SetterMethodMain {
+    public static void main(String[] args) {
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+ 
+        Country countryObj = (Country) appContext.getBean("CountryBean");
+        String countryName=countryObj.getCountryName();
+        String capitalName=countryObj.getCapital().getCapitalName();
+        System.out.println(capitalName+" is capital of "+countryName);
+      
+    }
+}
