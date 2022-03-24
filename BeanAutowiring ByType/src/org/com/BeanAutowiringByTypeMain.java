6c50a71055a1c8f9ad50d90921a758e197ce831c
Shubham Tadas <shubhamtadas828@gmail.com>
2022-03-24 16:22:19 +0530

    first commit

---
+++ BeanAutowiring ByType/src/org/com/BeanAutowiringByTypeMain.java
@@ -0,0 +1,13 @@
+package org.com;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class BeanAutowiringByTypeMain{
+     public static void main(String[] args) {
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Country countryObj = (Country) appContext.getBean("country");
+        String countryName=countryObj.getCountryName();
+        String capitalName=countryObj.getCapital().getCapitalName();
+        System.out.println(capitalName+" is capital of "+countryName);
+     }
+}
