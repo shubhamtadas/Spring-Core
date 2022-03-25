cc44b59c6adcaa5ab38cebfaa02b15740ce1a787
Shubham Tadas <shubhamtadas828@gmail.com>
2022-03-25 13:25:14 +0530

    first commit

--- springIOC/src/org/com/SetterMethodMain.java
+++ springIOC/src/org/com/SetterMethodMain.java
@@ -1,8 +1,8 @@
 package org.com;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.ClassPathResource;
 public class SetterMethodMain {
     public static void main(String[] args) {
         ApplicationContext appContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
--- springIOC2/src/org/com/ConstructorDIMain.java
+++ springIOC2/src/org/com/ConstructorDIMain.java
@@ -1,9 +1,9 @@
 package org.com;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.ClassPathResource;
  
 public class ConstructorDIMain{
  
