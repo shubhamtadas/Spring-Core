651a9a66972f0e6b360e752697365154c269d6b3
Shubham Tadas <shubhamtadas828@gmail.com>
2022-03-28 08:52:07 +0530

    initial commit

---
+++ LifetimeCallbacks/src/org/com/Country.java
@@ -0,0 +1,19 @@
+package org.com;
+
+public class Country {
+	String countryName ;
+	public String getCountryName() {	
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public void init()
+	{
+		System.out.println("In init block of country");
+	}
+	public void destroy()
+	{
+		System.out.println("In destroy block of country");
+	}
+}
---
+++ LifetimeCallbacks/src/org/com/LifetimeCallbacksMain.java
@@ -0,0 +1,11 @@
+package org.com;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class LifetimeCallbacksMain {
+	 public static void main(String[] args) {
+		 AbstractApplicationContext appContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		 Country countryObj = (Country) appContext.getBean("country");
+		 System.out.println("Country Name: "+countryObj.getCountryName());
+		 appContext.registerShutdownHook();
+		}
+}
